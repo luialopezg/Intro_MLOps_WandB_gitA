4771123b989c06c11929f9458654ea5be3f08047
Luis Alberto Lopez <luialopezg@gmail.com>
2023-11-26 10:10:39 -0500

    Primer commit: Descripción breve cambiios realizados

--- preprocessing.py
+++ preprocessing.py
@@ -3,12 +3,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 # Set path for the input
-RAW_DATA_DIR = os.environ["RAW_DATA_DIR"]
-RAW_DATA_FILE = os.environ["RAW_DATA_FILE"]
-raw_data_path = os.path.join(RAW_DATA_DIR, RAW_DATA_FILE)
+#RAW_DATA_DIR = os.environ["RAW_DATA_DIR"]
+#RAW_DATA_FILE = os.environ["RAW_DATA_FILE"]
+current_directory = os.getcwd()
+#raw_data_path = os.path.join(RAW_DATA_DIR, RAW_DATA_FILE)
 
 # Read dataset
+#df = pd.read_csv(raw_data_path, sep=",")
+raw_data_path = os.path.join(current_directory, "adult.csv")
 df = pd.read_csv(raw_data_path, sep=",")
+
 
 
 
@@ -45,10 +49,10 @@ train, test = train_test_split(data, test_size=0.3, stratify=data['income'])
 
 
 # Set path to the outputs
-PROCESSED_DATA_DIR = os.environ["PROCESSED_DATA_DIR"]
-train_path = os.path.join(PROCESSED_DATA_DIR, 'train.csv')
-test_path = os.path.join(PROCESSED_DATA_DIR, 'test.csv')
+#PROCESSED_DATA_DIR = os.environ["PROCESSED_DATA_DIR"]
+train_path = os.path.join(current_directory, 'train.csv')
+test_path = os.path.join(current_directory, 'test.csv')
 
 # Save csv
 train.to_csv(train_path, index=False)
-test.to_csv(test_path,  index=False)
+test.to_csv(test_path, index=False)
